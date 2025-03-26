f7d632912388f114b2f2d2bfa609e3f25449c017
Michal Strbka <Michal.Strbka@zuehlke.com>
2025-03-26 15:35:23 +0100

    removed not used method

--- Source/Core/Core.Abstractions/Geocoding/NoGeocoder.cs
+++ Source/Core/Core.Abstractions/Geocoding/NoGeocoder.cs
@@ -16,7 +16,7 @@ public class NoGeocoder : IGeocoder
     public Task<Either<ErrorStatus, Address>> Geocode(string location)
         => Task.FromResult<Either<ErrorStatus, Address>>(ErrorStatus.UnknownError);
 
-    public Task<Either<ErrorStatus, Address>> Geocode(string city, string? street = null, string? streeNumber = null, string? country = null)
+    public Task<Either<ErrorStatus, Address>> Geocode(string city, string? street = null, string? streetNumber = null, string? country = null)
         => Task.FromResult<Either<ErrorStatus, Address>>(ErrorStatus.UnknownError);
 
     public Task<Either<ErrorStatus, Address>> Geocode(LatLng latLng)
@@ -29,8 +29,5 @@ public class NoGeocoder : IGeocoder
         Task.FromResult<Either<ErrorStatus, StreetGeometry>>(ErrorStatus.UnknownError);
 
     public Task<Either<ErrorStatus, StreetGeometry>> GeocodeStreet(string city, string streetName) =>
-        Task.FromResult<Either<ErrorStatus, StreetGeometry>>(ErrorStatus.UnknownError);
-
-    public Task<Either<ErrorStatus, StreetGeometry>> GeocodeStreet(decimal latitude, decimal longitude) =>
         Task.FromResult<Either<ErrorStatus, StreetGeometry>>(ErrorStatus.UnknownError);
 }
